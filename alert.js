da2f5e9a533a8b54f60c1beaf2ea04e9b99f2549
nickcooldad <nickcooldad68@google.com>
2023-09-28 16:22:23 +0300

    feat: add help.js

--- alert.js
+++
@@ -1,9 +0,0 @@
-function caled() {
-  let sum
-  for (let i = 0; i < arguments.length; i++) {
-    // console.log(i)
-    sum = i + 1
-  }
-  return sum
-}
-console.log(caled(1, 2, 3, 6, 7))
---
+++ help.js
@@ -0,0 +1,13 @@
+// 1. открыть git bash
+// 2. ввести команду "git clone https://github.com/nickcooldad/rocket.git"
+// 3. выполнить "cd rocket"
+
+// ls - проверка наличия файлов в папке
+// cd name - переход вперед
+// cd .. - назад
+
+// git status - проверка статуса изменений
+// git add -A - перевести в промежуточные изменения
+// git commit -m "name commit" - закомитить изменения
+// git push - загрузка изменений на github
+// git pull - выгрузить изменения из хаба
