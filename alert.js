bf51f3a9192564f69c0608267c2341c6641c1717
nickcooldad <nickcooldad68@gmail.com>
2023-10-01 23:45:12 +0300

    homework (15)

--- alert.js
+++ alert.js
@@ -1,9 +1,14 @@
-function filter_list(arr) {
-  let newlist = []
-  for (let item of arr) {
-    if (typeof item === 'number') {
-      newlist.push(item)
-    }
+function findShort(s) {
+  let newfind = s.split(' , ')
+  for (let item of newfind) {
+    console.log(item)
+    console.log(item.length)
+    let i = 100
+    let num = item.length
+    if (num < i) i = num
+    console.log(i)
+    return i
   }
-  return newlist
 }
+
+findShort('bitcoin take over the world maybe who knows perhaps')
