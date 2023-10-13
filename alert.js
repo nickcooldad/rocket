9cd4fdc11d07dcf0d0b8a58429aacd9dcdc68604
nickcooldad <nickcooldad68@google.com>
2023-10-13 15:40:51 +0300

    Work111

--- alert.js
+++ alert.js
@@ -1,10 +1,24 @@
-function filterHomogenous(arrays) {
-  return arrays.filter(
-    item =>
-      item.length > 0 &&
-      item.every((elem, index, arr) => {
-        return typeof arr[0] === typeof arr[index]
-      }),
+function bingo(ticket, win) {
+  let arwin = ticket.filter(item =>
+    item.some((item, index, array) => {
+      return array[0].charCodeAt(index) === array[1]
+    }),
   )
+  return arwin.length >= win ? 'Winner!' : 'Loser!'
 }
-console.log(filterHomogenous([[1, 2, 3], ['a', 4], ['', '2'], []]))
+
+console.log(
+  bingo(
+    [
+      ['YFW', 73],
+      ['TCDWPO', 87],
+      ['XQ', 90],
+      ['JLEFYL', 87],
+      ['MMUG', 79],
+      ['NG', 81],
+      ['FB', 70],
+      ['OOLB', 86],
+    ],
+    2,
+  ),
+)
