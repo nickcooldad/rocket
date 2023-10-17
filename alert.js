82568346a414c6550cc1f0b61835d55b58726258
nickcooldad <nickcooldad68@google.com>
2023-10-17 14:04:48 +0300

    Work27-113

--- alert.js
+++ alert.js
@@ -1,27 +1,13 @@
-// reduce
-function rowWeights(array) {
-  let number1 = array
-    .filter((item, index, array) => {
-      if (index % 2 === 0) {
-        return array[index]
-      }
-    })
-    .reduce((acc, item, index, arr) => {
-      return (acc = acc + item)
-    }, 0)
-  let number2 = array
-    .filter((item, index, array) => {
-      if (index % 2 != 0) {
-        return array[index]
-      }
-    })
-    .reduce((acc, item, index, arr) => {
-      return (acc = acc + item)
-    }, 0)
-  //console.log(number1, number2)
+function scrollingText(text) {
+  let result = text.toUpperCase().split('')
   let ar = []
-  ar.push(number1, number2)
-  //console.log(ar)
+
+  result.map((item, index, array) => {
+    return ar.push(
+      result.slice(index).join('') + result.slice(0, index).join(''),
+    )
+  })
   return ar
 }
-console.log(rowWeights([80]))
+
+console.log(scrollingText('abc'))
