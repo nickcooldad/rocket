fc9c5f81afb1a41768ae783c992e45acdfddc7d6
nickcooldad <nickcooldad68@google.com>
2023-10-17 14:04:48 +0300

    Work27-113

--- zadachi.js
+++ zadachi.js
@@ -285,3 +285,15 @@ function rowWeights(array) {
   ar.push(number1, number2)
   return ar
 }
+//задача 27(113)
+function scrollingText(text) {
+  let result = text.toUpperCase().split('')
+  let ar = []
+
+  result.map((item, index, array) => {
+    return ar.push(
+      result.slice(index).join('') + result.slice(0, index).join(''),
+    )
+  })
+  return ar
+}
