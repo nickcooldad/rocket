b3b71ba28ff2cfcd2892a38bcc92ed99ad8dce26
nickcooldad <nickcooldad68@google.com>
2023-11-15 14:15:42 +0300

    Work42-210

--- zadachi.js
+++ zadachi.js
@@ -572,3 +572,18 @@ function indexEqualsValue(arr) {
 
   return result
 }
+// задача 42 (210)
+function validParentheses(parenStr) {
+  let curr = 0
+  for (let i = 0; i < parenStr.length; i++) {
+    if (parenStr[i] === '(') {
+      curr++
+    } else {
+      curr--
+    }
+    if (curr < 0) {
+      return false
+    }
+  }
+  return curr === 0
+}
