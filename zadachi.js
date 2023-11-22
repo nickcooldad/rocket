8884cd0b2f5a5037131eff0ccbf913349f1dbe3b
nickcooldad <nickcooldad68@gmail.com>
2023-11-22 21:07:31 +0300

    HomeWork45-213

--- zadachi.js
+++ zadachi.js
@@ -635,3 +635,15 @@ var runLengthEncoding = function (str) {
   }
   return result
 }
+//задача 45 (213)
+function isPrime(num) {
+  if (num < 2) {
+    return false
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true
+}
