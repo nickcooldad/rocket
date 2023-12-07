3de52797b1f5ef4e705079390f5b476780a3f6c9
nickcooldad <nickcooldad68@google.com>
2023-12-07 15:25:19 +0300

    Work54-302

--- zadachi.js
+++ zadachi.js
@@ -778,3 +778,22 @@ function DNAStrand(dna) {
     })
     .join('')
 }
+//Задача 54 (302)
+function scoreboard(string) {
+  let score = {
+    nil: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  }
+  return string
+    .split(' ')
+    .map(item => score[item])
+    .filter(elem => typeof elem === 'number')
+}
