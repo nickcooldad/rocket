cbf296e9c294e5ad49acddf422ca3b155a54762d
nickcooldad <nickcooldad68@gmail.com>
2024-01-10 19:16:09 +0300

    HomeWork89-512

--- zadachi.js
+++ zadachi.js
@@ -1299,3 +1299,22 @@ function once(fn) {
     }
   }
 }
+//задача 89 (512)
+function spyOn(func) {
+  let calls = 0
+  let all = []
+  let val
+
+  const spy = function (...args) {
+    calls++
+    all.push(...args)
+    val = func.apply(this, args)
+    return val
+  }
+
+  spy.callCount = () => calls
+  spy.wasCalledWith = x => all.some(a => x === a)
+  spy.returned = x => x === val
+
+  return spy
+}
