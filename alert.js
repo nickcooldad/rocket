c78aa3fa174728bda4be7cd241b95213a1cbff99
nickcooldad <nickcooldad68@gmail.com>
2024-03-29 22:15:06 +0300

    WOOORK

--- alert.js
+++ alert.js
@@ -1,22 +1,36 @@
-function fibonacciGenerator() {
-  let count = 0
-  let fibonacciArr = [0, 1]
+function primeGenerator() {
+  let num = 2
   return () => {
-    if(count > 1) {
-      fibonacciArr.push(fibonacciArr[count - 1] + fibonacciArr[count - 2])
-    }
-    return fibonacciArr[count++]
+    while(!isPrime(num)){
+     num++
+     }
+     return num++
   }
 }
-const gen = fibonacciGenerator();
 
-console.log(gen()); // 0
-console.log(gen()); // 1
-console.log(gen()); // 1
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
+
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
+
+
+const gen = primeGenerator();
+
 console.log(gen()); // 2
 console.log(gen()); // 3
 console.log(gen()); // 5
-console.log(gen()); // 8
+console.log(gen()); // 7
+console.log(gen()); // 11
 console.log(gen()); // 13
-console.log(gen()); // 21
-console.log(gen()); // 34
+console.log(gen()); // 17
+console.log(gen()); // 19
+console.log(gen()); // 23
+console.log(gen()); // 29
--- maxcode.js
+++ maxcode.js
@@ -264,3 +264,28 @@ function frequency(arr, options = defoltCompare) {
       return fibonacciArr[count++]
     }
   }
+
+
+  //функциональное программирование - 16 (First-class Citizens)
+  function primeGenerator() {
+    let num = 2
+    return () => {
+      while(!isPrime(num)){
+       num++
+       }
+       return num++
+    }
+  }
+  
+  
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+  
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+      if (num % i === 0) return false;
+    }
+  
+    return true;
+  }
