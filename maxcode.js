9f82d5e96022e40687764d029b0d2a7a7ebfb9a9
nickcooldad <nickcooldad68@gmail.com>
2024-04-23 21:43:16 +0300

    MWTK

--- maxcode.js
+++ maxcode.js
@@ -842,7 +842,20 @@ function clone(obj, link = new Map()) {
   }
   const newObj = {}
   for(let key in obj){
-    newObj[key] = link.has(key) ? link.get(key) : clone(obj[key], link.set(key, obj[key]))
+    newObj[key] = link.has(key) ? 
+    link.get(key) : clone(obj[key], link.set(key, obj[key]))
   }
   return newObj
 }
+
+//рекурсия 12
+function countChange(money, coins) {
+  if (money === 0){
+    return 1
+  }
+  if(money <= 0 || coins.length === 0) {
+    return 0
+  }
+  
+  return countChange(money - coins[0], coins) + countChange(money, coins.slice(1))
+  }
