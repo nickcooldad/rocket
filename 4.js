cbd1e3e3b63c40d951920c07aa0d6ce878b433e7
nickcooldad <nickcooldad68@gmail.com>
2024-05-04 12:38:50 +0300

    HMWRK

--- 4.js
+++ 4.js
@@ -8,34 +8,23 @@ function dfs(board, row, col, index, word){
 
   let memory = board[row][col]
   board[row][col] = ''
-
-  let result = dfs(board, row + 1, col , index + 1, word) ||
-  dfs(board, row - 1, col, index + 1, word) ||
-  dfs(board, row, col + 1, index + 1, word) ||
-  dfs(board, row, col - 1, index + 1, word) ||
-  dfs(board, row + 1, col + 1, index + 1, word) ||
-  dfs(board, row + 1, col - 1, index + 1, word) ||
-  dfs(board, row - 1, col + 1, index + 1, word) ||
-  dfs(board, row - 1, col - 1, index + 1, word)
-  
+  let result = [[row - 1,col], [row + 1, col], [row,col + 1], [row,col - 1], [row - 1,col - 1], [row - 1,col + 1], [row + 1,col - 1], [row + 1,col + 1]]
+    .some(([row, col]) => dfs(board, row, col, index +1, word))
   board[row][col] = memory
    
   return result
   }
 
-  function checkWord(board, word) {
-    let checkWords = false
-      for(let i = 0; i < board.length; i++){
-        for(let j = 0; j < board[i].length; j++){
-          if(word[0] === board[i][j]){
-            if(dfs(board, i, j, 0, word)){
-              checkWords = true
-            }
-          }
-        }
+function checkWord(board, word) {
+  for(let i = 0; i < board.length; i++){
+     for(let j = 0; j < board[i].length; j++){
+      if(dfs(board, i, j, 0, word)){
+        return true
       }
-    return checkWords
+    }
   }
+  return false
+}
 
   const board = [ 
     ["I","L","A","W"],
--- maxcode.js
+++ maxcode.js
@@ -1145,7 +1145,6 @@ function dfs(grid,row,col){
   dfs(grid, row - 1, col + 1) +
   dfs(grid, row - 1, col - 1)
 }
-
 //рекурсия 21
 function checkWord(board, word) {
   let checkWords = false
@@ -1181,4 +1180,32 @@ function checkWord(board, word) {
       }
     
     return checkWords
+  }
+  //
+  function dfs(board, row, col, index, word){
+    if (index === word.length){
+      return true
+    }
+    if(row < 0 || row > board.length - 1 || col < 0 || col > board[row].length - 1 || word[index] !== board[row][col] || board[row][col] === ''){
+      return false;
+    }
+  
+    let memory = board[row][col]
+    board[row][col] = ''
+    let result = [[row - 1,col], [row + 1, col], [row,col + 1], [row,col - 1], [row - 1,col - 1], [row - 1,col + 1], [row + 1,col - 1], [row + 1,col + 1]]
+      .some(([row, col]) => dfs(board, row, col, index +1, word))
+    board[row][col] = memory
+     
+    return result
+    }
+  
+  function checkWord(board, word) {
+    for(let i = 0; i < board.length; i++){
+       for(let j = 0; j < board[i].length; j++){
+        if(dfs(board, i, j, 0, word)){
+          return true
+        }
+      }
+    }
+    return false
   }
