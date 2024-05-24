a491117fe7e3b78b0063e69ccbf5ceb3b59189b3
nickcooldad <nickcooldad68@gmail.com>
2024-05-24 20:31:25 +0300

    HMMWRK

--- maxcode.js
+++ maxcode.js
@@ -1620,3 +1620,17 @@ Object.prototype.get = function(link) {
       patchObj = patchObj[key]
     });
   }
+  //ООП - 14
+  Array.prototype.map2 = function (callback, thisArg) {
+    const arr = Array(this.length)
+    for(let i = 0; i < this.length; i++){
+      if(i in this){
+        if(thisArg !== undefined){
+          arr[i] = callback.call(thisArg, this[i], i, this)
+        } else{
+          arr[i] = callback(this[i], i, this)
+        }
+      }
+    }
+    return arr
+  }
