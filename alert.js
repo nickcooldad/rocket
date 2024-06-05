351babd13302e52bcd27f20e4083ab9f52c06f3d
nickcooldad <nickcooldad68@gmail.com>
2024-06-05 21:31:39 +0300

    hmwrk

--- alert.js
+++ alert.js
@@ -1,11 +1,29 @@
-function race(iterable) {
- return new Promise((resolve, reject) =>{
-  for(const promis of iterable){
-    Promise.resolve(promis).then(resolve, reject)
-  }
- })
-   
+function all(promises) {
+  return new Promise((resolve, reject) => {
+    const cache = []
+    let firstCount = 0
+    let lastCount = 0
+
+    for (const promis of promises){
+      const index = firstCount
+      firstCount++
+
+      Promise.resolve(promis).then(value => {
+        cache[index] = value
+        lastCount++
+
+        if(lastCount === firstCount){
+          resolve(cache)
+        }
+      }).catch(eror => reject(eror))
+    }
+
+    if(firstCount === 0){
+      resolve(cache)
+    }
+  })
 }
+
 
 const rand = () => Math.random() * 2000;
 
@@ -14,7 +32,7 @@ const p2 = new Promise(r => setTimeout(r, rand(), "B"));
 const p3 = new Promise(r => setTimeout(r, rand(), "C"));
 const p4 = new Promise(r => setTimeout(r, rand(), "D"));
 
-race([p1, p2, p3, p4]).then(
+all([p1, p2, p3, p4]).then(
     value => console.log("1 >>>", value),
     reason => console.log("2 >>>", reason),
 );
--- maxcode.js
+++ maxcode.js
@@ -1782,3 +1782,61 @@ function race(iterable) {
    }
   })
  }
+
+ //Промисы - 6
+
+ function all(promises) {
+  return new Promise((resolve) => {
+    const cache = []
+    let firstCount = 0
+    let lastCount = 0
+
+    for (const promis of promises){
+      const index = firstCount
+      firstCount++
+
+      Promise.resolve(promis).then(value => {
+        cache[index] = value
+        lastCount++
+
+        if(lastCount === firstCount){
+          resolve(cache)
+        }
+      })
+    }
+
+    if(firstCount === 0){
+      resolve(cache)
+    }
+  })
+}
+
+//Промисы - 7
+
+function all(promises) {
+  return new Promise((resolve, reject) => {
+    const cache = []
+    let firstCount = 0
+    let lastCount = 0
+
+    for (const promis of promises){
+      const index = firstCount
+      firstCount++
+
+      Promise.resolve(promis)
+      .then(value => {
+        cache[index] = value
+        lastCount++
+
+        if(lastCount === firstCount){
+          resolve(cache)
+        }
+      })
+      .catch(eror => reject(eror))
+    }
+
+    if(firstCount === 0){
+      resolve(cache)
+    }
+  })
+}
