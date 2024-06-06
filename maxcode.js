fa86343c4580ea498a877e3123ad0c7a2ed7cdf0
nickcooldad <nickcooldad68@google.com>
2024-06-06 09:55:20 +0300

    WWRK

--- maxcode.js
+++ maxcode.js
@@ -1838,3 +1838,38 @@ function all(promises) {
     }
   })
 }
+
+//Промисы - 8
+function allSettled(iterable) {
+  return new Promise((resolve) => {
+    let cache = []
+    let firstCount = 0
+    let lastCount = 0
+    
+    for(const promis of iterable){
+      const index = firstCount
+      firstCount++
+      
+      Promise.resolve(promis)
+      .then(value => {
+        cache[index] = {status: 'fulfilled', value: value}
+         lastCount++
+
+         if(firstCount === lastCount){
+          resolve(cache)
+         }
+      }).catch(er => {
+        cache[index] = {status: 'rejected', reason: er}
+        
+        lastCount++
+        if(firstCount === lastCount){
+          resolve(cache)
+         }
+      })
+       
+    }
+    if(firstCount === 0){
+      resolve(cache)
+    }
+  })
+}
