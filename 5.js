8f32dbd62e1376a1f1245730157110a2abdc5b7e
nickcooldad <nickcooldad68@gmail.com>
2024-06-20 22:05:00 +0300

    HMWRK

--- 5.js
+++ 5.js
@@ -1,8 +1,21 @@
-async function run(fns, limit) {
-  let cache = []
-   for(let i = 0; i <= fns.length; i += limit){
-    let functionResult = await Promise.all(fns.slice(i, i + limit).map(async fn => await fn()))
-    cache.push(...functionResult)
-   }
-  return Promise.resolve(cache)
+function withRetry(fn, limit) {
+  return async (a, b) => {
+    const errors = []
+    let count = 0
+    let boolean = true
+
+    while(boolean){
+      try {
+        const resultFunction = await fn(a, b)
+        boolean = false
+        return resultFunction
+      } catch(error) {
+        errors.push(error)
+        count++
+        if(count === limit){
+          return Promise.reject(new AggregateError(errors, "Too Many Calls"))
+        }
+      }
+    }
+  }
 }
--- maxcode.js
+++ maxcode.js
@@ -2019,3 +2019,26 @@ async function run(fns, limit) {
    }
   return cache
 }
+
+// Промисы - 17
+function withRetry(fn, limit) {
+  return async (a, b) => {
+    const errors = []
+    let count = 0
+    let boolean = true
+
+    while(boolean){
+      try {
+        const resultFunction = await fn(a, b)
+        boolean = false
+        return resultFunction
+      } catch(error) {
+        errors.push(error)
+        count++
+        if(count === limit){
+          return Promise.reject(new AggregateError(errors, "Too Many Calls"))
+        }
+      }
+    }
+  }
+}
