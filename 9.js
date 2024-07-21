6ed920c0ff810bd160f0c8ba3853ba0e77d3db77
nickcooldad <nickcooldad68@gmail.com>
2024-07-21 18:35:43 +0300

    HMWRK

--- 9.js
+++ 9.js
@@ -1,21 +1,89 @@
-function findAllJavascriptFiles(folder, callback, result = []) {
-  let countFolder = 0
-  folder.size((limit) => {
-    for(let i = 0; i < limit; i++){
-      folder.read(i, (file) => {
-        if(typeof file === 'object'){
-          countFolder++
-          findAllJavascriptFiles(file, callback, result)
-        }
-        if(typeof file === 'string' && file.endsWith('.js')){
-          result.push(file)
-        }
-        if(i === limit - 1 && countFolder === 0 ){
-          callback(result)
-        }
-      })
-    }   
-  })
+// function findAllJavascriptFiles(folder, callback,) {
+//   let countFile = 0
+//   let result = []
+//   folder.size((limit) => {
+//     for(let i = 0; i < limit; i++){
+//       folder.read(i, (file) => {
+//         if(typeof file === 'object'){
+//          let innerFunct =  findAllJavascriptFiles(file, callback, result)
+//          Object.assign(result, innerFunct)
+//         }
+//         if(typeof file === 'string' && file.endsWith('.js')){
+//           result.push(file)
+//         }
+//         if(countFile === limit){
+//           callback(result)
+//         }
+//       })
+//     }   
+//   })
+// }
+
+// function findAllJavascriptFiles(folder, callback) {
+//   if (typeof folder === 'string') {
+//     if (folder.endsWith('.js')) {
+//       callback([folder])
+//     } else {
+//       callback([])
+//     }
+//     return
+//   }
+
+//   folder.size((limit) => {
+//     if (limit === 0) {
+//       callback([])
+//     }
+//     let cache = []
+//     let countFile = 0
+//     for (let i = 0; i < limit; i++) {
+//       folder.read(i, (file) => {
+//         findAllJavascriptFiles(file, (result) => {
+//           cache.push(...result)
+//           countFile++
+//           if (countFile === limit) {
+//             callback(cache)
+//           }
+//         })
+//       })
+//     }
+//   })
+// }
+
+
+
+async function findAllJavascriptFilesPromise(folder) {
+  if (typeof folder === 'string') {
+    if (folder.endsWith('.js')) {
+      return [folder]
+    }
+    return []
+  }
+  
+  const read = promisify(folder.read);
+  const size = promisify(folder.size);
+
+  const promises = []
+  const limit = await size();
+
+  for (let i = 0; i < limit; i++) {
+    promises.push(read(i).then(findAllJavascriptFilesPromise));
+  }
+
+  const result = await Promise.all(promises);
+  return result.flat();
+}
+
+function promisify(fn) {
+  return (...args) => {
+    return new Promise((resolve) => {
+      fn(...args, resolve);
+    });
+  };
+}
+
+
+function findAllJavascriptFiles(folder, callback) {
+  findAllJavascriptFilesPromise(folder).then(arr => callback(arr));
 }
 
 
@@ -31,13 +99,16 @@ function Folder(files) {
 
 const root = Folder([
   "1.js",
+  "qwerty.txt",
   "2.js",
   Folder([
     Folder([
+      Folder([]),
       "3.txt",
     ]),
     "4.js",
   ]),
+  "8.html",
   Folder([
     "5.png",
     "6.js",
@@ -45,8 +116,15 @@ const root = Folder([
       "7.txt",
     ]),
   ]),
-  "8.html",
 ]);
+// const root = Folder([
+//   "1.js",
+//   "2.js",
+//   "3.txt",
+//   "4.js",
+//   "8.html"
+// ]);
+
 
 findAllJavascriptFiles(root, arr => {
   console.log(arr); // arr === ["1.js", "2.js", "4.js", "6.js"]
--- alert.js
+++ alert.js
@@ -1,41 +1,106 @@
-class AA {
-  #array;
-
-  constructor(...array) {
-    this.#array = array;
-  }
-
-  read() {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(this.#array), 100);
-    });
-  }
+async function filterProducts(catalog, minPrice, maxPrice) {
+  return [];
 }
 
 
-const example = new AA(
-  new AA(1, new AA(new AA(2)), new AA(3)),
-  new AA(4, new AA(5, 6), 7),
-  8,
-  new AA(9, new AA(10, new AA(11, 12), 13)),
-)
+const catalog = new Category("Catalog", true, [
+  new Category("Electronics", true, [
+    new Category("Smartphones", true, [
+      new Product("Smartphone 1", true, 1000),
+      new Product("Smartphone 2", true, 900),
+      new Product("Smartphone 3", false, 900),
+      new Product("Smartphone 4", true, 900),
+      new Product("Smartphone 5", true, 900)
+    ]),
+    new Category("Laptops", true, [
+      new Product("Laptop 1", false, 1200),
+      new Product("Laptop 2", true, 900),
+      new Product("Laptop 3", true, 1500),
+      new Product("Laptop 4", true, 1600)
+    ]),
+  ]),
+  new Category("Books", true, [
+    new Category("Fiction", false, [
+      new Product("Fiction book 1", true, 350),
+      new Product("Fiction book 2", false, 400)
+    ]),
+    new Category("Non-Fiction", true, [
+      new Product("Non-Fiction book 1", true, 250),
+      new Product("Non-Fiction book 2", true, 300),
+      new Product("Non-Fiction book 3", true, 400)
+    ]),
+  ]),
+]);
 
-async function flatten(value) {
-  const result = [];
+const minPrice = 300, maxPrice = 1500;
 
-  for (const element of await value.read()) {
-    if (element instanceof AA) {
-      result.push(...await flatten(element));
-    } else {
-      result.push(element);
+filterProducts(catalog, minPrice, maxPrice)
+  .then(products => console.log(products));
+
+
+  function helper(cb, value, ok = true, ms = 10) {
+    setTimeout(() => {
+      if (ok) {
+        cb(null, value); // success
+      } else {
+        cb("error"); // bad luck
+      }
+    }, ms);
+  }
+  
+  class Category {
+    #name;
+    #inStock;
+    #children;
+  
+    constructor(name, inStock, children) {
+      this.#name = name;
+      this.#inStock = inStock;
+      this.#children = children;
+    }
+  
+    getName(cb) {
+      helper(cb, this.#name);
+    }
+    checkInStock(cb) {
+      helper(cb, this.#inStock);
+    }
+    getChildren(cb) {
+      helper(cb, this.#children);
     }
   }
-  return result;
-}
+  
+  class Product {
+    #name;
+    #inStock;
+    #price;
+  
+    constructor(name, inStock, price) {
+      this.#name = name;
+      this.#inStock = inStock;
+      this.#price = price;
+    }
+  
+    getName(cb) {
+      helper(cb, this.#name);
+    }
+    checkInStock(cb) {
+      helper(cb, this.#inStock);
+    }
+    getPrice(cb) {
+      helper(cb, this.#price);
+    }
+  }
+  
 
-console.time("aa")
-flatten(example).then((result) => {
-  console.log(result); // [1,2,3,4,5,6,7,8,9,10,11,12,13]
-  console.timeEnd("aa"); // ≈ 1000 ms
-});
-
+  // products === [  
+  //   { name: "Non-Fiction book 2", price: 300 },  
+  //   { name: "Non-Fiction book 3", price: 400 },  
+  //   { name: "Laptop 2", price: 900 },  
+  //   { name: "Smartphone 2", price: 900 },  
+  //   { name: "Smartphone 4", price: 900 },  
+  //   { name: "Smartphone 5", price: 900 },  
+  //   { name: "Smartphone 1", price: 1000 },  
+  //   { name: "Laptop 3", price: 1500 }  
+  // ]
+  
