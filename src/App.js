a64b9b131fb03e4308a9b9c7611850d9dc5f5dc5
nickcooldad <nickcooldad68@gmail.com>
2024-07-30 22:19:20 +0300

    hmwrk

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import "./App.css"
+import Counter from './components/counter';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [count, setCount] = useState(0)
+
+  const counter = () => {
+    setCount( count + 1)
+  }
+  
+
+
+  return ( 
+    <div className="home">
+      <h1 className='title'>Поймано покемонов</h1>
+      <h1 className='counter'>{`${count} /1053`}</h1>
+      <div className='note'>
+      <Counter className="note"/>
+      <Counter className="note"/>
+      <Counter className="note"/>
+      <Counter className="note"/>
+      <Counter className="note"/>
+      <Counter className="note"/>
+      <Counter className="note"/>
+      <Counter className="note"/>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/counter.jsx
@@ -0,0 +1,20 @@
+
+import "./counter.css"
+const Counter = () => {
+
+    
+    return (
+        <div className="card">
+            <h1 className="title">name</h1>
+            <div className="photo">
+            <img className="resized-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" />
+            </div>
+            <button className="botton">Отпустить</button>
+        </div>
+    )
+}
+
+export default Counter
+
+
+
