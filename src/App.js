6aa7454c6a1419670f294667b558cd4a094e6eb4
nickcooldad <nickcooldad68@gmail.com>
2024-07-31 22:49:49 +0300

    Update learn submodule

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import Counter from './components/counter';
 function App() {
   const [count, setCount] = useState(0)
 
-  const counter = () => {
-    setCount( count + 1)
-  }
   
 
 
@@ -18,16 +15,19 @@ function App() {
       <h1 className='title'>Поймано покемонов</h1>
       <h1 className='counter'>{`${count} /1053`}</h1>
       <div className='note'>
-      <Counter className="note"/>
-      <Counter className="note"/>
-      <Counter className="note"/>
-      <Counter className="note"/>
-      <Counter className="note"/>
-      <Counter className="note"/>
-      <Counter className="note"/>
-      <Counter className="note"/>
+        <p1 className="line1">
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      </p1>
+        <p1 className="line2">  
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      </p1>
       </div>
-      
     </div>
   );
 }
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,11 +5,11 @@ const Counter = () => {
     
     return (
         <div className="card">
-            <h1 className="title">name</h1>
+            <h1 className="titleName">name</h1>
             <div className="photo">
             <img className="resized-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" />
             </div>
-            <button className="botton">Отпустить</button>
+            <button className="botton">ОТПУСТИТЬ</button>
         </div>
     )
 }
