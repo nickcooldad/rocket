98a9248d4ce96d729c9f39df1967aa212345da6a
nickcooldad <nickcooldad68@gmail.com>
2024-08-04 21:29:41 +0300

    HMWK

--- src/App.js
+++ src/App.js
@@ -2,30 +2,108 @@
 import { useState } from 'react';
 import "./App.css"
 import {Counter, Sum} from './components/counter';
-
+const pokemons = [
+  {
+    name: "bulbasaur",
+    id: "1"
+  },
+  {
+    name: "ivysaur",
+    id: "2"
+  },
+  {
+    name: "venusaur",
+    id: "3"
+  },
+  {
+    name: "charmander",
+    id: "4"
+  },
+  {
+    name: "charmeleon",
+    id: "5"
+  },
+  {
+    name: "charizard",
+    id: "6"
+  },
+  {
+    name: "squirtle",
+    id: "7"
+  },
+  {
+    name: "wartortle",
+    id: "8"
+  },
+  {
+    name: "blastoise",
+    id: "9"
+  },
+  {
+    name: "caterpie",
+    id: "10"
+  },
+  {
+    name: "metapod",
+    id: "11"
+  },
+  {
+    name: "butterfree",
+    id: "12"
+  },
+  {
+    name: "weedle",
+    id: "13"
+  },
+  {
+    name: "kakuna",
+    id: "14"
+  },
+  {
+    name: "beedrill",
+    id: "15"
+  },
+  {
+    name: "pidgey",
+    id: "16"
+  },
+  {
+    name: "pidgeotto",
+    id: "17"
+  },
+  {
+    name: "pidgeot",
+    id: "18"
+  },
+  {
+    name: "rattata",
+    id: "19"
+  },
+  {
+    name: "raticate",
+    id: "20"
+  }
+]
 
 function App() {
   const [count, setCount] = useState(0)
   const incrementCount = () => {
     setCount(count + 1)
   }
+  
+  const dicrementCount = () => {
+    setCount(count - 1)
+  }
+
   return ( 
     <div className="home">
       <h className='title'>Поймано покемонов</h>
-      <Sum count={count}/>
-      <div className='note'>
-        <p1 className="line1">
-      <Counter incrementCount={incrementCount}/>
-      <Counter incrementCount={incrementCount}/>
-      <Counter incrementCount={incrementCount}/>
-      <Counter incrementCount={incrementCount}/>
-      </p1> 
-        <p1 className="line2">  
-      <Counter incrementCount={incrementCount}/>
-      <Counter incrementCount={incrementCount}/>
-      <Counter incrementCount={incrementCount}/>
-      <Counter incrementCount={incrementCount}/>
-      </p1>
+      <h1 className='counter'>{`${count} / ${pokemons.length}`}</h1>
+      <div className='note'>{
+        pokemons.map(pokemon => {
+          return <Counter incrementCount={incrementCount} dicrementCount={dicrementCount} id={pokemon.id} name={pokemon.name}/>
+        })
+        }
       </div>
     </div>
   );
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,132 +3,31 @@ import { useState } from 'react';
 import "./counter.css"
 
 
-const Counter = ({incrementCount}) => {
-const names = [
-    {
-      name: "bulbasaur",
-      id: "1"
-    },
-    {
-      name: "ivysaur",
-      id: "2"
-    },
-    {
-      name: "venusaur",
-      id: "3"
-    },
-    {
-      name: "charmander",
-      id: "4"
-    },
-    {
-      name: "charmeleon",
-      id: "5"
-    },
-    {
-      name: "charizard",
-      id: "6"
-    },
-    {
-      name: "squirtle",
-      id: "7"
-    },
-    {
-      name: "wartortle",
-      id: "8"
-    },
-    {
-      name: "blastoise",
-      id: "9"
-    },
-    {
-      name: "caterpie",
-      id: "10"
-    },
-    {
-      name: "metapod",
-      id: "11"
-    },
-    {
-      name: "butterfree",
-      id: "12"
-    },
-    {
-      name: "weedle",
-      id: "13"
-    },
-    {
-      name: "kakuna",
-      id: "14"
-    },
-    {
-      name: "beedrill",
-      id: "15"
-    },
-    {
-      name: "pidgey",
-      id: "16"
-    },
-    {
-      name: "pidgeotto",
-      id: "17"
-    },
-    {
-      name: "pidgeot",
-      id: "18"
-    },
-    {
-      name: "rattata",
-      id: "19"
-    },
-    {
-      name: "raticate",
-      id: "20"
+export const Counter = ({ incrementCount, dicrementCount, name, id }) => {
+
+  const [caught, setCaught] = useState(false)
+
+  const hundleClick = () => {
+    setCaught(!caught)
+    
+    if (!caught) {
+      incrementCount()
+    } else {
+      dicrementCount()
     }
-  ]
-const [clickState, setClickState] = useState('ПОЙМАТЬ')
-const [currenIndex, setCurrentIndex] = useState(0)
-
-const hundleClick = () => {
-  if(clickState === "ПОЙМАТЬ")
-    {
-    incrementCount()
-    setClickState('ОТПУСТИТЬ')
-  } else{
-    setClickState('ПОЙМАТЬ')
-    setCurrentIndex((currenIndex + 1) % names.length)
   }
+
+  return (
+    <div className={caught ? 'pokemon caught' : 'pokemon'}>
+      <h1 className="titleName">{name}</h1>
+      <div className="photo">
+        <img className="resized-image" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" />
+      </div>
+      <button className="botton" onClick={hundleClick}>{caught ? "ОТПУСТИТЬ" : 'ПОЙМАТЬ'}</button>
+    </div>
+  )
 }
 
-    return (
-        <div className={clickState === 'ПОЙМАТЬ' ? 'card1' : 'card2'}>
-            <h1 className="titleName">{names[currenIndex].name}</h1>
-            <div className="photo">
-            <img className="resized-image" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${currenIndex + 1}.png`} alt="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" />
-            </div>
-            < BottonState value={clickState} handleClick={hundleClick}/>
-        </div>
-    )
-}
 
-
-
-const BottonState = ({value, handleClick}) => {
-    return (
-        <>
-            <button className="botton" onClick={ handleClick}>{value}</button>
-        </>
-    )
-}
-
-const Sum = ({count}) =>{
-    return (
-        <>
-        <h1 className='counter'>{`${count} /1053`}</h1>
-        </>
-    )
-}
-
-export  {Counter, Sum }
 
 
