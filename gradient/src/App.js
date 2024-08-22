44fc687da4640b8ba00015ec4a7a18da775c8ba2
nickcooldad <nickcooldad68@gmail.com>
2024-08-22 21:57:13 +0300

    hmwrk

--- gradient/src/App.js
+++ gradient/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-const [iputValue, setInputValue] = useState({rgb1:'#FFFFFF', rgb2 : '#FFFFFF'})
 const [tempValue, setTempValue] = useState({rgb1:'#FFFFFF', rgb2 : '#FFFFFF'})
+const [backGroundColor, setBackGroundColor] = useState(`linear-gradient(to bottom, white, white)` )
 
-
+useEffect(() => {
+  document.body.style.background = backGroundColor
+}, [backGroundColor])
 const hundleChangeFirst = (event) =>{
   setTempValue(prev => ({...prev, rgb1: event.target.value}))
 }
@@ -17,12 +19,13 @@ const hundleChangeLast = (event) => {
 }
 const hundleSubmit = (event) =>{
   event.preventDefault();
-  setInputValue(tempValue)
+  setBackGroundColor(`linear-gradient(to bottom, ${tempValue.rgb1}, ${tempValue.rgb2})`)
 
 }
+//style={{ background: `linear-gradient(to bottom, ${tempValue.rgb1} 50%, ${tempValue.rgb2} 50%)` }
 return (
 
-    <div className="buttonsRgb" style={{ background: `linear-gradient(to bottom, ${tempValue.rgb1} 50%, ${tempValue.rgb2} 50%)` }}>
+    <div className="buttonsRgb" >
         <div className='inputs'>
         <input className="firstColor" placeholder='RGB' onChange={hundleChangeFirst}/>
         <input className="lastColor" placeholder='RGB' onChange={hundleChangeLast}/>
