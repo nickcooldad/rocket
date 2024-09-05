981170f91ec654d28f7f535b6b4f1187c2d28d61
nickcooldad <nickcooldad68@google.com>
2024-09-05 16:44:32 +0300

    hmwrk

--- src/test/getLastPageNumber.js
+++
@@ -1,4 +0,0 @@
-
-export function getLastPageNumber(count, size) {
-    return Math.ceil(count / size) - 1;
-}
--- table-attedance/src/visitPerson/vacationDay.js
+++
@@ -1,5 +0,0 @@
-export function vacationDay (vacation, day, month, year){
-    return vacation[year] !== undefined 
-    && vacation[year][month] !== undefined 
-    && vacation[year][month].has(day)
-  }
